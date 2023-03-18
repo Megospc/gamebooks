2013cdf5c3696923faa9f5fa3db61ef1702f3c09
Megospc <97227980+Megospc@users.noreply.github.com>
2023-03-18 13:28:56 +0300

    Add files via upload

--- game1/script.js
+++ game1/script.js
@@ -1,1 +1,15 @@
-
+var obj = {
+  options: {
+    music: "music.mp3",
+    onstart: () => {
+      room('start');
+    }
+  },
+  assets: [],
+  rooms: [
+    { id: "start", f: function() {
+      println('Скоро выйдет. Краткое описание.\nОтодвинув ковёр в одной из комнат\nдома номер «24», вы попадаете в крысиное\nгосударство. Около года назад из него\nпропал король. Теперь в нём творятся\nжуткие беспорядки...\nИграйте также:');
+      opt('Чан в сарае', () =>  { open('https://megospc.github.io/gamebooks/game0/gamebook.html') });
+    } }
+  ]
+};
