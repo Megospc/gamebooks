3b7feabe1d48dedad7d91ae07dc96b675dd4f1c3
Megospc <97227980+Megospc@users.noreply.github.com>
2023-04-01 11:08:26 +0300

    Add files via upload

--- game2/script.js
+++ game2/script.js
@@ -1,11 +1,12 @@
 var obj = {
   nmae:  "Домик в горах",
   options: {
-    music: "music.mp3",
+    music: "test/music.mp3",
     onstart: () => {
       println('Скоро выйдет. Краткое описание:\n«Вы попадаете в домик-отель на верхушке одной\nиз гор. С виду он вполне безопасный, но\nкто знает, какие тайны он скрывает...»\nИграйте также:');
       opt('Чан в сарае', () =>  { open('https://megospc.github.io/gamebooks/game0/gamebook.html') });
       opt('Тайник под ковром', () =>  { open('https://megospc.github.io/gamebooks/game1/gamebook.html') });
+      opt('Играть в пре-альфа версию', () =>  { open('test/gamebook.html') });
     }
   },
   assets: [],
